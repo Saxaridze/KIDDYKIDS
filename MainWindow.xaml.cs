d4c78e71f6d9f4accc848dc65829e02b5432bf88
Caxar{Оk} <volckov.danila-volk@yandex.ru>
2023-04-04 23:57:39 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Media;
+using System.IO;
+using System.ComponentModel;
+using System.Windows.Markup;
+using System.Globalization;
+
+namespace KIDDYKIDS
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SoundPlayer Player = new SoundPlayer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Manager.MainFrame = MainFrame;
+            MainFrame.Navigate(new PageMain());
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (!MainFrame.CanGoBack)
+                BtnBack.Visibility = Visibility.Collapsed;
+            else
+                BtnBack.Visibility = Visibility.Visible;
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+                MainFrame.GoBack();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnTurnONMusic_Click(object sender, RoutedEventArgs e)
+        {
+            this.Player.SoundLocation = "TwirlyTops.wav";
+            this.Player.Load();
+            this.Player.PlayLooping();
+            BtnTurnONMusic.Visibility = Visibility.Hidden;
+            BtnTurnOFFMusic.Visibility = Visibility.Visible;
+        }
+
+        private void BtnTurnOFFMusic_Click(object sender, RoutedEventArgs e)
+        {
+            this.Player.Stop();
+            BtnTurnOFFMusic.Visibility = Visibility.Hidden;
+            BtnTurnONMusic.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ PageMain.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KIDDYKIDS
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMain.xaml
+    /// </summary>
+    public partial class PageMain : Page
+    {
+        public PageMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGamesForKids_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("PageGamesForKids.xaml", UriKind.Relative));
+
+        }
+
+        private void BtnEducationalGames_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGamesForAttention_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLogicAndThinkingGames_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnColoringPages_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPuzzles_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRiddles_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
